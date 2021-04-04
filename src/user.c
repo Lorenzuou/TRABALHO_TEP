a9645acacfea3fbeab902f72753c295e43db1a6a
LORENZO SCARAMUSSA <lorenzoscaramussa@hotmail.com>
2021-04-04 18:18:47 -0300

    resolvido
    Co-authored-by: henrickbart <henrickbart@users.noreply.github.com>

--- src/user.c
+++ src/user.c
@@ -1,50 +1,44 @@
 #include "user.h"
 
-int criarMenuLogin()
+char *lerLinha()
 {
-    char entrada;
-    printf("**LOCADORA VIRTUAL**\n\n1.Login\n2.Cadastro\n3.Sair\n");
-    scanf("%c", &entrada);
+    int caracter;
+    size_t tamanhoChunk = TAM;
+    size_t posicaoChunk = 0;
 
-    switch (entrada)
+    char *string = malloc(TAM);
+
+    if (string == NULL)
     {
-    case '1':
-
-        return realizarLogin();
-
-        break;
-    case '2':
-
-        if (realizarCadastro())
-        {
-            return realizarLogin();
-        }
-        else
-        {
-            return 0;
-        }
-        break;
-    case '3':
-        return 2;
-        break;
-    case '4':
-
-        break;
-
-    default:
-        break;
+        printf("Não há espaço para ser alocado em memória\n");
+        exit(EXIT_FAILURE);
     }
 
-    // system("clear");
-    return 0;
+    while ((caracter = fgetc(stdin)) != '\n' && caracter != '\r')
+    {
+        string[posicaoChunk++] = caracter;
+        if (posicaoChunk == tamanhoChunk)
+        {
+            tamanhoChunk += TAM;
+
+            string = realloc(string, tamanhoChunk);
+
+            if (string == NULL)
+            {
+                printf("Não há mais espaço para ser alocado em memória\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    string[posicaoChunk] = 0;
+
+    return string;
 }
 
 Usuario *carregarUsuarios()
 {
-
     Usuario *user;
     user = (Usuario *)malloc(sizeof(Usuario) * TAM);
-    int countUsuario = 0;
     qtdUsuarios = 0;
 
     FILE *file = fopen("data/usuarios.csv", "r");
@@ -53,15 +47,19 @@ Usuario *carregarUsuarios()
 
     while (fgets(linha, 1024, file))
     {
-
-        strcpy(user[countUsuario].nome, strtok(linha, ","));
-        strcpy(user[countUsuario].senha, strtok(NULL, ","));
+        if (qtdUsuarios + 1 == TAM)
+        {
+            user = realloc(user, sizeof(Usuario) * TAM * 2);
+        }
+        user[qtdUsuarios].nome = malloc(sizeof(char) * TAM);
+        user[qtdUsuarios].senha = malloc(sizeof(char) * TAM);
+        strcpy(user[qtdUsuarios].nome, strtok(linha, ","));
+        strcpy(user[qtdUsuarios].senha, strtok(NULL, ","));
 
         // printf("1:%s, ", user[countUsuario].nome);
         // printf("2:%s\n", user[countUsuario].senha);
 
         qtdUsuarios++;
-        countUsuario++;
     };
 
     fclose(file);
@@ -71,43 +69,56 @@ Usuario *carregarUsuarios()
 
 int realizarLogin()
 {
-
     Usuario *usuarios = carregarUsuarios();
 
-    char nome[TAM];
-    char senha[TAM];
-    system("clear");
+    char *nome;
+    char *senha;
+
+    //system("clear");
+
+    printf("-------------\n");
+    printf("    LOGIN    \n");
+    printf("-------------\n\n");
 
     printf("Usuario: ");
-    scanf("%s", nome);
+    getchar(); // pegar o enter do input de usuario
+    nome = lerLinha();
+
     printf("\nSenha: ");
-    scanf("%s", senha);
+    senha = lerLinha();
 
     for (int i = 0; i < qtdUsuarios; i++)
     {
         if (!strcmp(nome, usuarios[i].nome)) // verifica se há um usuario com o que foi digitado
         {
+            int retorno = 0;
+
             // printf("%s - %s\n", nome, usuarios[i].nome);
             // printf("%s - %s", senha, usuarios[i].senha);
             usuarios[i].senha[strcspn(usuarios[i].senha, "\n")] = 0; // Toma como nulo a posição da string que ocorrer um "\n"
 
             if (!strcmp(senha, usuarios[i].senha)) // verifica se a senha do usuario confere.
             {
-                system("clear");
-                free(usuarios);
-                return 1;
+                retorno = 1;
             }
             else
             {
                 system("clear");
-                printf("\nSenha incorreta. ");
-                free(usuarios);
-                return 0;
+                printf("Senha incorreta.\n");
+
+                retorno = 0;
             }
+
+            free(usuarios);
+            free(nome);
+            free(senha);
+
+            return retorno;
         }
     }
+    
     system("clear");
-    printf("\nUsuario nao cadastrado.");
+    printf("Usuário não cadastrado.\n");
 
     free(usuarios);
     return 0;
@@ -115,31 +126,65 @@ int realizarLogin()
 
 int realizarCadastro()
 {
-    Usuario novoUsuario;
-
     Usuario *usuarios = carregarUsuarios();
-    char confSenha[30];
+
+    Usuario novoUsuario;
+    char *confSenha;
+
+    system("clear");
+
+    printf("------------\n");
+    printf("  CADASTRO  \n");
+    printf("------------\n\n");
 
     printf("Digite seu nome de usuario: ");
-    scanf("%s", novoUsuario.nome);
+    getchar(); // pegar o enter do input de usuario
+    novoUsuario.nome = lerLinha();
+
     printf("\nDigite sua senha: ");
-    scanf("%s", novoUsuario.senha);
+    // pegar o enter do input de usuario
+    novoUsuario.senha = lerLinha();
+
     printf("\nConfirme sua senha: ");
-    scanf("%s", confSenha);
+    confSenha = lerLinha();
+
+    printf("%s\n", novoUsuario.nome);
+    printf("%s\n", novoUsuario.senha);
+    printf("%s\n", confSenha);
 
     for (int i = 0; i < qtdUsuarios; i++)
     {
-
         if (!strcmp(novoUsuario.nome, usuarios[i].nome)) // verifica se há um usuario com o que foi digitado
         {
             system("clear");
-            printf("\nNome de usuario já utilizado.");
+            printf("Usuário já cadastrado.\n");
+            
             return 0;
         }
     }
 
     if (!strcmp(confSenha, novoUsuario.senha))
     {
+        int posicao = 0;
+        char caracter;
+
+        while (caracter != '\n')
+        {
+            caracter = novoUsuario.senha[posicao];
+
+            if (!isalnum(caracter))
+            {
+                system("clear");
+                printf("Senha fora do padrão.\n");
+
+                return 0;
+            }
+
+            printf("%c\n", caracter);
+
+            posicao++;
+        }
+
         FILE *file = fopen("data/usuarios.csv", "a");
 
         fprintf(file, "\n%s,%s", novoUsuario.nome, novoUsuario.senha);
@@ -147,8 +192,16 @@ int realizarCadastro()
 
         fclose(file);
 
-        printf("\n Usuario cadastrado com sucesso!\n \n Pressione enter para continuar!");
-        getchar(); 
+        system("clear");
+        printf("Usuário cadastrado com sucesso!\nInsira as informações para fazer o login:\n");
+
         return 1;
     }
+    else
+    {
+        system("clear");
+        printf("As senhas não conferem.\n");
+    }
+
+    return 0;
 }
